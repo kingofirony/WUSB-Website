098b7904ca05a9655d44f8b426f481bd260cb166
Ken Fehling <ken@androidideas.org>
2016-03-22 21:42:31 -0400

    Fixed bug in typeahead

--- public/js/site/post-program.js
+++ public/js/site/post-program.js
@@ -3,7 +3,7 @@ var djs = new Bloodhound({
 	queryTokenizer: Bloodhound.tokenizers.whitespace,
 	// url points to a json file that contains an array of country names, see
 	// https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
-	prefetch: 'api/users'
+	prefetch: '/api/users'
 });
 
 // passing in `null` for the `options` arguments will result in the default
