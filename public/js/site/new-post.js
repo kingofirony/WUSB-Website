0308b815a0969ad6d2a51aafe12f1fa65de59041
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2016-03-29 00:05:09 -0400

    why is there no jQuery support for css3 anime

--- public/js/site/new-post.js
+++ public/js/site/new-post.js
@@ -1,5 +1,34 @@
 $('#new-post').click(function() {
-	$(this).addClass('row');
-	$(this).css({"width": "inherit", "max-height": "1000px"});
-	$(this).find("form").removeClass("hidden");
+	var plus = $("span.plus");
+	var form = $(this).find("form");
+
+	
+	plus.bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
+		
+		if (plus.hasClass('rotate-right')){
+			plus.css({'transform' : 'rotate(45deg)'});
+			plus.removeClass('rotate-right');
+		}
+		else if (plus.hasClass('rotate-left')) {
+			plus.css({'transform': 'rotate(0deg)'});
+			plus.removeClass('rotate-left');
+		}
+		
+	});
+
+
+	$(this).toggleClass('row');
+
+	if (form.hasClass("hidden")) {
+		$(this).css({"width": "inherit", "height": "auto"});
+		form.removeClass("hidden");
+		plus.addClass('rotate-right');
+
+	}
+	else {
+		$(this).css({"width": "50px", "height": "50px"});
+		form.addClass('hidden');
+		plus.addClass('rotate-left');
+
+	}
 });
