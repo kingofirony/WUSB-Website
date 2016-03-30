c59e8e6a39c8c11f4b788919f9e060556ee5b4d5
Ken Fehling <ken@androidideas.org>
2016-03-29 23:31:59 -0400

    isPublished, optional title / name - date

--- routes/post/post_playlist.js
+++ routes/post/post_playlist.js
@@ -39,7 +39,7 @@ exports = module.exports = (req, res) => {
 			updater.process(data, {
 				flashErrors: true,
 				logErrors: true,
-				fields: 'title, description'
+				fields: 'title, description, isPublished'
 			}, err => {
 				if (err) return reject(err);
 				addTracksToPlaylist(playlist, data);
