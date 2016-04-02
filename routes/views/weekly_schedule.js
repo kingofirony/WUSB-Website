3fb97c0d7e50e56ecace9197b30d36c2507dea6d
Ken Fehling <ken@androidideas.org>
2016-04-01 22:43:13 -0400

    Got basic weekly schedule working

--- routes/views/weekly_schedule.js
+++ routes/views/weekly_schedule.js
@@ -1,13 +1,22 @@
 'use strict';
 const keystone = require('keystone');
+const _ = require('underscore');
 const Program = keystone.list('Program');
 
 exports = module.exports = (req, res) => {
 	const view = new keystone.View(req, res);
 	const locals = res.locals;
 	locals.section = 'schedule';
-	locals.days = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
-	locals.timeslots = [ '1200', '1230' ];
-
+	locals.days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	locals.timeslots = Program.model.getTimeSlots();
+	locals.calendar = {};
+	_.each(_.range(7), i => {
+		locals.calendar[locals.days[i]] = {};
+		_.each(locals.timeslots, timeslot => {
+			Program.model.findBySlot(i, timeslot.number).exec().then(p => {
+				locals.calendar[locals.days[i]][timeslot.number] = p
+			});
+		});
+	});
 	view.render('weekly_schedule');
 };
