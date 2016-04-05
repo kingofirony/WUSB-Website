5cb806bb3df26b14a1dbeead6d6aded0c2a74350
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2016-04-05 16:34:21 -0400

    fix posts

--- public/js/site/new-post.js
+++ public/js/site/new-post.js
@@ -2,5 +2,4 @@ $('#new-post').click(function() {
 	$(this).addClass('row');
 	$(this).css({"width": "inherit", "max-height": "1000px"});
 	$(this).find("form").removeClass("hidden");
-	console.log("clicked");
 });
