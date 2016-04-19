1a1a048a47cbd7300333b94f9af2e59a5eee4d2a
Peter Geiss <peter.w.geiss@gmail.com>
2016-04-19 17:18:14 -0400

    Fix crash when adding playlist

--- models/Playlist.js
+++ models/Playlist.js
@@ -6,6 +6,7 @@ const Types = keystone.Field.Types;
 
 // Docs: http://keystonejs.com/docs/database/#lists-options
 const options = {
+	autokey: { path: 'slug', from: '_id', unique: true },
 	singular: 'Playlist',
 	plural: 'Playlists',
 	sortable: true,
@@ -65,13 +66,21 @@ Playlist.schema.pre('save', function (next) {
 	if ((this.isNew && this.isPublished) || 
 	 (this.isModified('program') && this.isPublished) ||
 	 (this.isModified('isPublished') && this.isPublished)) {
-		keystone.list('Program').model.update(
-			{ _id: this.program }, 
-			{ $push: { playlists: this } },
-			function (err) {
+		const Program = keystone.list('Program');
+		Program.model.update(
+			{ playlists: this._id }, 
+			{ $pull: { playlists: this._id } },
+			{ multi: true },
+			(err, out) => {
 				if (err) throw err;
-			}
-		);
+				Program.model.findOneAndUpdate(
+					{ _id: this.program }, 
+					{ $push: { playlists: this._id } },
+					(err) => {
+						if (err) throw err;
+					}
+				);
+		});
 	}
 	next();
 });
@@ -81,7 +90,7 @@ Playlist.schema.pre('update', function (next) {
 	  (this.isModified('isPublished') && this.isPublished)) {
 	  	const Program = keystone.list('Program');
 		Program.model.update(
-			{ $in: { playlists: this._id } }, 
+			{ playlists: this._id },
 			{ $pull: { playlists: this._id } },
 			{ multi: true },
 			(err, out) => {
