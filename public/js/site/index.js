51aa11358356e379ca58e9926c3d56e58ad1d0eb
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2016-05-08 22:37:41 -0400

    cards expand on click

--- public/js/site/index.js
+++ public/js/site/index.js
@@ -1,9 +1,21 @@
 
+var $grid = $('.grid').masonry({
+	itemSelector: '.grid-item'
+});
+
 $('.card').each(function() {
 	console.log(this);
 	if (this.offsetHeight < this.scrollHeight) {
-		$(this).css("background", "linear-gradient(white 310px, grey)");
+		$(this).addClass("card-overflow");
+		
+		$(this).click(function() {
+			console.log("clicked");
+			console.log($grid);
+			$(this).parent().toggleClass("grid-item--expand");
+			$grid.masonry();
+		});
 	} else {
 		// your element doesn't have overflow
 	}
+	
 });
