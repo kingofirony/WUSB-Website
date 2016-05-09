da36a4abc6ada088966d2273afff06ddfab05fce
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2016-05-09 00:12:57 -0400

    when resized add or remove expand class

--- public/js/site/index.js
+++ public/js/site/index.js
@@ -1,21 +1,30 @@
 
-var $grid = $('.grid').masonry({
-	itemSelector: '.grid-item'
+function expandCards() {
+	var $grid = $('.grid').masonry({
+		itemSelector: '.grid-item'
+	});
+
+	$('.card').each(function () {
+		console.log(this);
+		if (this.offsetHeight < this.scrollHeight) {
+			$(this).addClass("card-overflow");
+
+			$(this).click(function () {
+				console.log("clicked");
+				console.log($grid);
+				$(this).parent().toggleClass("grid-item--expand");
+				$grid.masonry();
+			});
+		} else {
+			$(this).removeClass("card-overflow");
+		}
+	});
+}
+
+$(window).resize(function() {
+	expandCards();
 });
 
-$('.card').each(function() {
-	console.log(this);
-	if (this.offsetHeight < this.scrollHeight) {
-		$(this).addClass("card-overflow");
-		
-		$(this).click(function() {
-			console.log("clicked");
-			console.log($grid);
-			$(this).parent().toggleClass("grid-item--expand");
-			$grid.masonry();
-		});
-	} else {
-		// your element doesn't have overflow
-	}
-	
+$(document).ready(function() {
+	expandCards();
 });
